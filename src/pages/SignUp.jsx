1f4c8ba077f26b690d38ee15172f301c567b44d5
kmihailo <mihailo.krstic@eng.it>
2024-07-24 14:42:20 +0200

    Save user to firebase

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,7 +2,9 @@ import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
-
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
@@ -21,6 +23,39 @@ function SignUp() {
       [e.target.id]: e.target.value,
     }))
   }
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await createUserWithEmailAndPassword
+        (
+          auth,
+          email,
+          password
+        )
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+
+      const formDataCopy = { ...formData }
+      delete formDataCopy.password
+      formDataCopy.timestamp = serverTimestamp()
+
+
+      // const userRef = doc(db, 'users', user.uid );
+      // await  setDoc(userRef, formDataCopy);
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+      navigate('/')
+    } catch (error) {
+      console.log("error")
+    }
+  }
 
   return (
     <>
@@ -30,8 +65,8 @@ function SignUp() {
             Welcome!
           </p>
         </header>
-        <form>
-        <input
+        <form onSubmit={onSubmit}>
+          <input
             type="text"
             className="nameInput"
             placeholder="Name"
@@ -60,7 +95,7 @@ function SignUp() {
               onClick={() => setShowPassword((prevState) => !prevState)}
             />
           </div>
-             <div className="signUpBar">
+          <div className="signUpBar">
             <p className="signUpText">
               Sign Up
             </p>
