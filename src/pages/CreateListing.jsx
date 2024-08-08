26fda258c17e7c826169202baa68856a64590884
kmihailo <mihailo.krstic@eng.it>
2024-08-09 01:21:27 +0200

    Upload images to firebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,8 +1,18 @@
 import { useState, useEffect, useRef } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import {
+    getStorage,
+    ref,
+    uploadBytesResumable,
+    getDownloadURL
+} from 'firebase/storage'
+import { db } from '../firebase.config'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
 import { useNavigate } from 'react-router-dom'
-import { Spinner } from '../components/Spinner'
 import { toast } from 'react-toastify'
+import {v4 as uuidv4} from 'uuid'
+import { Spinner } from '../components/Spinner'
+
 
 const CreateListing = () => {
     const [geolocationEnabled, setGeolocationEnabled] = useState(false)
@@ -58,7 +68,7 @@ const CreateListing = () => {
         }
     }, [isMounted])
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault()
         setLoading(true)
 
@@ -74,17 +84,67 @@ const CreateListing = () => {
             return
         }
 
-        let geoLocation = {}
+        let geolocation = {}
         let location
 
         if (geolocationEnabled) {
             // geo location nisam zavrsio zbog platne kartice
         } else {
-            geoLocation.lat = latitude
-            geoLocation.lng = longitude
+            geolocation.lat = latitude
+            geolocation.lng = longitude
             location = address
         }
-        setLoading(false)
+
+        // Store image in firebase
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage()
+                const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+
+                const storageRef = ref(storage, 'images/' + fileName)
+
+                const uploadTask = uploadBytesResumable(storageRef, image)
+
+                uploadTask.on(
+                    'state_changed',
+                    (snapshot) => {
+                        const progress =
+                            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                        console.log('Upload is ' + progress + '% done')
+                        switch (snapshot.state) {
+                            case 'paused':
+                                console.log('Upload is paused')
+                                break
+                            case 'running':
+                                console.log('Upload is running')
+                                break
+                            default:
+                                break
+                        }
+                    },
+                    (error) => {
+                        reject(error)
+                    },
+                    () => {
+                        // Handle successful uploads on complete
+                        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                            resolve(downloadURL)
+                        })
+                    }
+                )
+            })
+        }
+
+        const imageUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+        ).catch(() => {
+            setLoading(false)
+            toast.error('Images not uploaded')
+            return
+        })
+
+        
     }
 
     const onMutate = (e) => {
