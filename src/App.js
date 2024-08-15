411c7c0eb095447129a3514c46d4f895a009b67b
kmihailo <mihailo.krstic@eng.it>
2024-08-15 15:09:32 +0200

    Fetch single listing

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import CreateListing from "./pages/CreateListing";
 import ForgotPassword from './pages/ForgotPassword'
+import Listing from "./pages/Listing";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
 
@@ -27,6 +28,7 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/category/:categoryName/:listingId'element={<Listing />}/>
         </Routes>
         <Navbar />
       </Router>
