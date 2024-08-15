f9fbaf9b2caab2d424da96087268201ecfe986af
kmihailo <mihailo.krstic@eng.it>
2024-08-15 17:28:14 +0200

    Contact Landlord page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import SignUp from './pages/SignUp'
 import CreateListing from "./pages/CreateListing";
 import ForgotPassword from './pages/ForgotPassword'
 import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
 
@@ -29,6 +30,7 @@ function App() {
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
           <Route path='/category/:categoryName/:listingId'element={<Listing />}/>
+          <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
